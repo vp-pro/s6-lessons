065eed0f3b3900acbb2434075a9f5a3268708c6b
Pavel Kulagov <kulagov@yandex-team.ru>
2022-07-12 08:23:18 +0300

    upd de06020501

--- Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica/Задание 1/realization.py
+++ Тема 2. Аналитические СУБД. Vertica/5. Запись данных в Vertica/Задание 1/realization.py
@@ -1,8 +1,1 @@
-conn_user = {
-    'host': '51.250.75.20',
-    'port': 5433,
-    'user': '***', # введите свой username в Vertica      
-    'password': '***', # введите свой password в Vertica      
-    'database': 'dwh',
-    'autocommit': True
-}
+vertica_user = 'укажите здесь свой логин'
