0d34b3c2cebb5ef1a97e42504accbcec0e6ba845
Pavel Kulagov <kulagov@yandex-team.ru>
2022-07-15 13:39:16 +0300

    add de06021001

---
+++ Тема 2. Аналитические СУБД. Vertica/10. Выбираем параметры сортировки/Задание 1/submit.py
@@ -0,0 +1,28 @@
+import os
+ 
+import requests
+
+def submit(t_code, rlz_file=''):
+    user_code = ''
+    if rlz_file:
+        full_lesson_path = os.path.dirname(os.path.abspath(__file__))
+        user_file = f'{full_lesson_path}/{rlz_file}'
+
+        with open(user_file, 'r') as u_file:
+            user_code = u_file.read()
+
+    r = requests.post(
+        'http://localhost:3002',
+        json={
+            "code": user_code,
+            "test": t_code
+            })
+
+    print(r.json()['stderr'].replace('__test',rlz_file[:-3]))
+    print(r.json()['stdout'].replace('__test',rlz_file[:-3]))
+
+if __name__ == '__main__':
+    submit(
+        'de06021001',
+        'realization.py'
+    )
