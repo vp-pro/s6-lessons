d921f74d2c62337232f32be82d8d082add188923
Pavel Kulagov <kulagov@yandex-team.ru>
2022-07-15 20:44:24 +0300

    upd de06021101

---
+++ Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/realization.sql
@@ -0,0 +1,11 @@
+SET SESSION AUTOCOMMIT TO off;
+
+DELETE FROM xxx WHERE xxx > 123;
+
+SELECT ххх, ххх, ххх FROM DELETE_VECTORS
+	where ххх like 'users%';
+
+SELECT func(ххх) FROM DELETE_VECTORS
+	where xxx like 'users%';
+
+ROLLBACK;
--- Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/submit.py
+++ Тема 2. Аналитические СУБД. Vertica/11. Удаление и обновление данных в Vertica/Задание 1/submit.py
@@ -2,7 +2,7 @@ import os
  
 import requests
 
-def submit(t_code, rlz_file=''):
+def submit(t_code, rlz_file='', conn=''):
     user_code = ''
     if rlz_file:
         full_lesson_path = os.path.dirname(os.path.abspath(__file__))
@@ -11,11 +11,19 @@ def submit(t_code, rlz_file=''):
         with open(user_file, 'r') as u_file:
             user_code = u_file.read()
 
+    if conn:
+        full_lesson_path = os.path.dirname(os.path.abspath(__file__))
+        user_file = f'{full_lesson_path}/conn.py'
+
+        with open(user_file, 'r') as u_file:
+            user_conn = u_file.read()
+
     r = requests.post(
         'http://localhost:3002',
         json={
             "code": user_code,
-            "test": t_code
+            "test": t_code,
+            "conn": user_conn
             })
 
     print(r.json()['stderr'].replace('__test',rlz_file[:-3]))
@@ -24,5 +32,6 @@ def submit(t_code, rlz_file=''):
 if __name__ == '__main__':
     submit(
         'de06021101',
-        'realization.py'
+        'realization.sql',
+        'conn.py'
     )
