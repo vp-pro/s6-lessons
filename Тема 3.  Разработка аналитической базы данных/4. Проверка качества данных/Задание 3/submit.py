97a0fe70042c526643d41aea94c3b034cb531c43
Pavel Kulagov <kulagov@yandex-team.ru>
2022-07-25 23:14:24 +0300

    add de06030403

---
+++ Тема 3.  Разработка аналитической базы данных/4. Проверка качества данных/Задание 3/submit.py
@@ -0,0 +1,31 @@
+import os
+ 
+import requests
+
+def submit(t_code, rlz_file=''):
+    user_code = ''
+    if rlz_file:
+        full_lesson_path = os.path.dirname(os.path.abspath(__file__))
+        user_file = f'{full_lesson_path}/{rlz_file}'
+
+        with open(user_file, 'r') as u_file:
+            user_code = u_file.read()
+
+    r = requests.post(
+        'http://localhost:3002',
+        json={
+            "code": user_code,
+            "test": t_code
+            })
+    if 'sql' in rlz_file:
+        print(r.json()['stderr'].replace('__test',rlz_file[:-4]).replace('.py','.sql'))
+        print(r.json()['stdout'].replace('__test',rlz_file[:-4]).replace('.py','.sql'))
+    else:
+        print(r.json()['stderr'].replace('__test',rlz_file[:-3]))
+        print(r.json()['stdout'].replace('__test',rlz_file[:-3]))
+
+if __name__ == '__main__':
+    submit(
+        'de06030403',
+        'realization.sql'
+    )
